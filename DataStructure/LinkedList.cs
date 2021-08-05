1c5e5ae3ed6e49eccf3458df6cef0aab764013f5
Akash-Patil-12 <patilakash121212@gmail.com>
2021-08-05 15:51:17 +0530

    [Akash] Add: Implemented created a simple linked list

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+   public class LinkedList
+    {
+        internal Node head;
+        /// <summary>
+        /// Add new data into linked list
+        /// </summary>
+        /// <param name="data"></param>
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                 //   Console.WriteLine("Node :" + temp.data);
+                    temp = temp.next;
+                }
+                temp.next = node;
+
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(30);
+            list.Add(40);
+            list.Add(50);
+        }
+    }
+}
